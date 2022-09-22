b5bb907ece645b65de711471cada851774175bed
Lawrence <kan6666king@gmail.com>
2022-09-22 14:31:30 +0600

    cleaning up the project

--- functions.php
+++ functions.php
@@ -1,21 +1,9 @@
 <?php
 
-function print_arr(array $array)
-{
-    echo ("<pre>");
-    var_dump($array);
-    echo ("</pre>");
-}
-
+// validation functions
 function clear_data($data)
 {
-    return trim(
-        stripcslashes(
-            htmlspecialchars(
-                $data
-            )
-        )
-    );
+    return trim(stripcslashes(htmlspecialchars($data)));
 }
 
 function check_min_length(string $string, int $length): bool
@@ -23,30 +11,4 @@ function check_min_length(string $string, int $length): bool
     return strlen($string) >= $length;
 }
 
-function fill_article_arr(array &$article): array
-{
-    foreach ($article as $key => &$value) {
-        $value['assets'] = clear_data($_POST['article_' . $key]);
-    }
-    return $article;
-}
-
-function check_article_arr(array &$article, array $length = ['title' => 5, 'body' => 15]): array
-{
-    foreach ($article as $key => &$value) {
-        $value['errors']['min_length'] = check_min_length($value['assets'], 5) == true ?: "Минимальная длина должна быть " . $length['title'] . " символов";
-    }
-    return $article;
-}
-
-function split_errors(array $errors): string
-{
-    $errors_template = array();
-    $div = ['start' => '<div>', 'end' => '</div>'];
-
-    foreach ($errors as $key => &$value) {
-        $errors_template[] = $div['start'] . $value . $div['end'];
-    }
-    return implode($errors_template);
-}
 
---
+++ view/form.php
@@ -0,0 +1,12 @@
+<div class="container bg-white pt-5">
+    <form action="../add.php" method="post">
+        <label for="article_title">Title:</label><br>
+        <input type="text" name="article_title"><br><br>
+
+        <label for="article_body">Article:</label><br>
+        <input type="text" name="article_body"><br><br>
+
+        <button type="submit">Submit</button>
+    </form>
+    <br><br><br><br>
+</div>
